6381a59f7e6f9c66209273ba941526fd68857806
muliqunluan <17823167416@163.com>
2026-07-15 10:26:24 +0800

    fix: 修复无法启动的问题

--- src/roles/dto/update-role.dto.ts
+++ src/roles/dto/update-role.dto.ts
@@ -1,8 +1,10 @@
 // src/roles/dto/update-role.dto.ts
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateRoleDto {
+  @ApiProperty({ description: '角色名称', example: 'editor' })
   @IsString()
   @IsNotEmpty()
-  name: string;
+  name!: string;
 }
